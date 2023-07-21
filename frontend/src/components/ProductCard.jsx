cf36796d608456d299aeb057dba05027b018baf7
Shivam Dubey <108024596+Shivam00002@users.noreply.github.com>
2023-07-21 23:23:40 +0530

    added something

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -16,6 +16,7 @@ export const ProductCard = () => {
   return (
     <Card maxW="sm">
       <CardBody>
+
         <Image
           src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
           alt="Green double couch with wooden legs"
@@ -29,6 +30,7 @@ export const ProductCard = () => {
           </Text>
         </Stack>
       </CardBody>
+      
       <Divider />
       <CardFooter>
         <ButtonGroup spacing="2">
