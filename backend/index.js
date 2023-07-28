1f1b89e0f5e090d4a95616285a1dc489978255e8
Shivam Dubey <108024596+Shivam00002@users.noreply.github.com>
2023-07-28 22:07:10 +0530

    added backend

---
+++ backend/index.js
@@ -0,0 +1,91 @@
+if (process.env.NODE_ENV != "production") {
+    require("dotenv").config();
+}
+const express = require("express");
+
+const app = express();
+const cors = require("cors");
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+
+const mysql = require("mysql");
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "sellit",
+});
+
+
+db.connect(function (error) {
+
+    if (error) {
+        throw error;
+    } else {
+        console.log("MySQL Database is connected Successfully");
+    }
+});
+
+
+app.get('/', (req, res) => {
+    res.send("Welcome to our Website")
+})
+
+app.post('/signup', (req, res) => {
+    let sqlData = [req.body.email, req.body.password]
+    let sql = "INSERT INTO `signup`( `email`, `password`) VALUES (?,?)"
+    db.query(sql, sqlData, (error, result) => {
+        if (error) {
+            return res.status("error is comeing")
+        } else {
+            return res.json(result)
+        }
+    })
+})
+
+app.get("/signup", (req, res) => {
+    let sql = "SELECT * FROM signup"
+    db.query(sql, (error, result) => {
+        if (error) {
+            return res.status("error is comeing")
+        } else {
+            return res.json(result)
+        }
+    })
+})
+
+
+
+app.get("/signup/:id", (req, res) => {
+    let userID = req.params.id
+    let sql = "SELECT * FROM signup WHERE email =?"
+    db.query(sql, userID , (error , result)=>{
+        if (error) {
+            return res.status("error is comeing")
+        } else {
+            return res.json(result)
+        }
+    } )
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(8000);
--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,33 +1,68 @@
-import React from 'react'
-import { Navbar } from '../components/Navbar'
-import { Button, Checkbox, Container, FormControl, FormLabel, Input, Textarea } from '@chakra-ui/react'
-
-
+import React from "react";
+import { Navbar } from "../components/Navbar";
+import {
+  Button,
+  Checkbox,
+  Container,
+  FormControl,
+  FormLabel,
+  Input,
+  Textarea,
+} from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
 
 function Admin() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <>
-   <Navbar/>
-   <Container maxW={"container.xl"} display={"grid"} placeContent={"center"} mt={3} >
-   <FormControl minW={600}>
-  <FormLabel>Title</FormLabel>
-  <Input type='text'  mb={5}/>
-  <FormLabel>Desc</FormLabel>
-  <Textarea placeholder='Product Description...' mb={5}/>
-  <FormLabel>Price</FormLabel>
-  <Input  mb={5}/>
-  <FormLabel>Title</FormLabel>
-  <Input type='text'  mb={5}/>
-  <FormLabel>Quantity</FormLabel>
-  <Input type='number'  mb={5}/>
-<FormLabel>Product Image</FormLabel>
-<Input type='file' mb={5} />
-<Checkbox defaultChecked>Cash on delivery</Checkbox>
-<Button colorScheme='blue' mt={2} width={'full'}>Submit</Button>
-</FormControl>
-   </Container>
+      <Navbar />
+      <Container
+        maxW={"container.xl"}
+        display={"grid"}
+        placeContent={"center"}
+        mt={3}
+      >
+        <FormControl minW={600}>
+          <FormLabel>Title</FormLabel>
+          <Input type="text" {...register("Title")} mb={5} />
+          <FormLabel>Desc</FormLabel>
+          <Textarea
+            placeholder="Product Description..."
+            {...register("Product decription")}
+            mb={5}
+          />
+          <FormLabel>Price</FormLabel>
+          <Input mb={5} {...register("Price")} />
+          <FormLabel>Title</FormLabel>
+          <Input type="text" {...register("P")} mb={5} />
+          <FormLabel>Quantity</FormLabel>
+          <Input type="number" {...register("Product Quetity")} mb={5} />
+          <FormLabel>Product Image</FormLabel>
+          <Input type="text" {...register("Product Image")} mb={5} />
+          <Checkbox defaultChecked>Cash on delivery</Checkbox>
+          <Button
+            method="post"
+            onClick={handleSubmit(onSubmit)}
+            colorScheme="blue"
+            mt={2}
+            width={"full"}
+          >
+            Submit
+          </Button>
+        </FormControl>
+      </Container>
     </>
-  )
+  );
 }
 
-export default Admin
+export default Admin;
