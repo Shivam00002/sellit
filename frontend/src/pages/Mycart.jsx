40e74a7ad0ac6d2e6cb3a71970a495e0ccefb306
Shivam00002 <dubey02shiv@gmail.com>
2023-08-08 14:40:59 +0530

    added

--- frontend/src/pages/Mycart.jsx
+++ frontend/src/pages/Mycart.jsx
@@ -1,8 +1,63 @@
-import React from "react";
+import React, { useContext } from 'react';
+import {
+  Button,
+  ButtonGroup,
+  Card,
+  CardBody,
+  CardFooter,
+  Divider,
+  Heading,
+  Image,
+  Stack,
+  Text,
+} from '@chakra-ui/react';
+import { CartContext } from '../ContextProvider';
 
 const Mycart = () => {
-  return <div>Mycart</div>;
-};
+  const { cartItem, setCartItem } = useContext(CartContext);
 
+  const handleDelete = (productId) => {
+    const updatedCart = cartItem.filter((product) => product.id !== productId);
+    setCartItem(updatedCart);
+  };
+
+  return (
+    <Card w="full">
+      {cartItem.map((product) => (
+        <CardBody key={product.id}>
+          <Image
+            src={product.cod}
+            alt={product.title}
+            borderRadius="lg"
+            height={100}
+            width={100}
+          />
+          <Stack mt="6" spacing="3">
+            <Heading size="md">{product.title}</Heading>
+            <Text>
+              <b>Description:</b> {product.description}
+            </Text>
+            <Text color="blue.600" fontSize="2xl">
+              Rs.{product.price}
+            </Text>
+          </Stack>
+          <Divider />
+
+          <CardFooter>
+            <ButtonGroup spacing="2">
+              <Button
+                variant="ghost"
+                colorScheme="red"
+                onClick={() => handleDelete(product.id)} 
+              >
+                Delete from cart
+              </Button>
+            </ButtonGroup>
+          </CardFooter>
+        </CardBody>
+      ))}
+    </Card>
+  );
+};
 
 export default Mycart;
